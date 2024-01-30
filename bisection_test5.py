d13c65d5684be7d24e4eb08ef1d0a1238c49e8bb
sison-lab <153555712+sison-lab@users.noreply.github.com>
2024-01-30 12:58:47 -0500

    Update bisection_test5.py

--- bisection_test5.py
+++ bisection_test5.py
@@ -1,9 +1,9 @@
-import bisection;
+import secant;
 
 # Test whether it will terminate if it can't find the root
 
 def example_func(x):
     return 3 + x**2
 
-def test_bisection():
-    assert bisection.bisection_algorithm(example_func, 0, 1, .01) == "Could Not Locate Root"
+def test_secant():
+    assert secant.secant_algorithm(example_func, 0, 1, .01) == "Could Not Locate Root"
