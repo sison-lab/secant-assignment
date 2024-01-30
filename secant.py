f323926bbb3bcbd3a0e767378cd8d4157821b16d
sison-lab <153555712+sison-lab@users.noreply.github.com>
2024-01-30 14:12:09 -0500

    Update secant.py

--- secant.py
+++ secant.py
@@ -2,4 +2,4 @@ def secant_algorithm(function, left_endpoint, right_endpoint, error_tolerance):
     guess = left_endpoint
     error = function(guess)
     number_of_tries = 0
-    return guess, error, number_of_tries
+    return guess, error
