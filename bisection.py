69cbac5a8131eec83dc1f692548cc000316c41c4
sison-lab <153555712+sison-lab@users.noreply.github.com>
2024-01-30 12:57:34 -0500

    Update bisection.py

--- bisection.py
+++ bisection.py
@@ -1,4 +1,4 @@
-def bisection_algorithm(function, left_endpoint, right_endpoint, error_tolerance):
+def secant_algorithm(function, left_endpoint, right_endpoint, error_tolerance):
     guess = left_endpoint
     error = function(guess)
     number_of_tries = 0
