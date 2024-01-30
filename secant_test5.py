329192a443cf3d744eeaca35d10532b55e271909
sison-lab <153555712+sison-lab@users.noreply.github.com>
2024-01-30 14:11:02 -0500

    Update secant_test5.py

--- secant_test5.py
+++ secant_test5.py
@@ -6,4 +6,4 @@ def example_func(x):
     return 3 + x**2
 
 def test_secant():
-    assert secant.secant_algorithm(example_func, 0, 1, .01) == "Could Not Locate Root"
+    assert secant.secant_algorithm(example_func, 0, 5, .01) == "Could Not Locate Root"
