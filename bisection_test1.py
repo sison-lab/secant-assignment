681c27d9cfc2c087a478e63793f3f9fc879f5551
sison-lab <153555712+sison-lab@users.noreply.github.com>
2024-01-30 13:01:07 -0500

    Update bisection_test1.py

--- bisection_test1.py
+++ bisection_test1.py
@@ -1,10 +1,10 @@
-import bisection;
+import secant;
 
 def example_func(x):
     return 3-x**2
 
-def test_bisection():
-    location, error, number_of_tries = bisection.bisection_algorithm(example_func, 0, 2, .01)
+def test_secant():
+    location, error, number_of_tries = secant.secant_algorithm(example_func, 0, 2, .01)
     assert location - 1.734375 < .00001
     assert abs(error) < .01
     assert number_of_tries == 7
