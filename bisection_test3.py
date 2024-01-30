8a895a5c17cb9f3f365328a617072b898ed2c0a6
sison-lab <153555712+sison-lab@users.noreply.github.com>
2024-01-30 13:00:01 -0500

    Update bisection_test3.py

--- bisection_test3.py
+++ bisection_test3.py
@@ -1,9 +1,9 @@
-import bisection;
+import secant;
 
 # Test whether it will work when solution is left endpoint
 
 def example_func(x):
     return 1-x**2
 
-def test_bisection():
-    assert bisection.bisection_algorithm(example_func, 1, 2, .01) == (1, 0, 0)
+def test_secant():
+    assert secant.secant_algorithm(example_func, 1, 2, .01) == (1, 0, 0)
